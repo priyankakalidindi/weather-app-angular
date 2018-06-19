692afdb3b5ec2edf6304f50ea2bcc840bc81c8c3
Priyanka Koteshwari Kalidindi <priyankakalidindi23@gmail.com>
2018-06-19 15:36:20 +0530

    except chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { WeatherService } from './weather.service';
+import { ISummary } from './models/summary';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  // httpService: Http;
+  // constructor(httpService: Http) {
+  //   this.httpService = httpService;
+  // }
+  city: string;
+  constructor(public weatherService: WeatherService) {
+  }
+
+  onButtonClick(cityName: string) {
+    console.log(cityName);
+    this.weatherService.fetchWeatherInfo(cityName);
+    // console.log(this.city);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
+import { WeatherService } from './weather.service';
+import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+import { ChartsModule } from 'ng2-charts/ng2-charts';
 import { AppComponent } from './app.component';
+import { SummaryComponent } from './component/summary/summary.component';
+import { TemperatureComponent } from './component/temperature/temperature.component';
+import { TpwComponent } from './component/tpw/tpw.component';
+import { ChartComponent } from './component/chart/chart.component';
+import { DayTileComponent } from './component/day-tile/day-tile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryComponent,
+    TemperatureComponent,
+    TpwComponent,
+    ChartComponent,
+    DayTileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    ChartsModule
   ],
-  providers: [],
+  providers: [WeatherService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
